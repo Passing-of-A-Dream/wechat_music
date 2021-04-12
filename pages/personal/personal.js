a185f2307b4801c76f97ee9fe293f4888feeecba
路过的哆啦A梦 <80303513+Passing-of-A-Dream@users.noreply.github.com>
2021-04-12 10:49:39 +0800

    新增登录

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,11 +1,15 @@
 // pages/personal/personal.js
+let statrY = 0; //手指起始的坐标
+let moveY = 0;  //手指移动的坐标
+let moveDistance = 0;  //手指移动的距离
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    coverTransform: 'translateY(0)',
+    coveTransition: ''
   },
 
   /**
@@ -13,6 +17,35 @@ Page({
    */
   onLoad: function (options) {
 
+  },
+
+  handleTouchStart(event){
+    this.setData({
+      coveTransition: ''
+    })
+    // 获取手指起始坐标
+    statrY = event.touches[0].clientY;
+  },
+  handleTouchMove(event){
+    moveY = event.touches[0].clientY;
+    moveDistance = moveY - statrY; //获取移动的距离
+
+    if (moveDistance <= 0) {
+      return;
+    }
+    if (moveDistance >= 90) {
+      moveDistance = 90;
+    }
+    // 动态更新coverTransform的状态值
+    this.setData({
+      coverTransform: `translateY(${moveDistance}rpx)`
+    })
+  },
+  handleTouchEnd(){
+    this.setData({
+      coverTransform: `translateY(0rpx)`,
+      coveTransition: 'transform 0.3s linear'
+    })
   },
 
   /**
