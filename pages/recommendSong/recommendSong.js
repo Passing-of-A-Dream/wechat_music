e87c012c156004a9c5b5d92b6e9bd0cf80f7b9b3
路过的哆啦A梦 <80303513+Passing-of-A-Dream@users.noreply.github.com>
2021-04-17 15:33:12 +0800

    新增了一点小东西

--- pages/recommendSong/recommendSong.js
+++ pages/recommendSong/recommendSong.js
@@ -1,19 +1,53 @@
 // pages/recommendSong/recommendSong.js
+import requst from '../../utils/request'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    day: '',  // 天
+    month: '',  // 月
+    recommendList: [], //推荐列表数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 判断用户是否登录
+    let userInfo = wx.getStorageSync('userInfo');
+    if (!userInfo) {
+      wx.showToast({
+        title: '请先登录',
+        icon: 'none',
+        success: ()=>{
+          // 跳转至登录界面
+          wx.reLaunch({
+            url: '/pages/login/login'
+          });
+        }
+      });
+    }
+    // 更新日期的状态数据
+    this.setData({
+      day: new Date().getDate(),
+      month: new Date().getMonth() + 1
+    })
 
+    // 获取每日推荐的数据
+    this.getRecommendList();
   },
+
+  // 获取用户每日推荐数据
+  getRecommendList(){
+    requst('/recommend/songs').then((res) => {
+      this.setData({
+        recommendList: res.recommend
+      })
+    })
+  },
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
